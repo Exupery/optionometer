41b07c7104b8a5992de589dcfebb0f63081f84d3
Matthew Frost <mf@matthewfrost.com>
2013-08-18 15:19:24 -0400

    added mysql driver

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,14 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "mysql" % "mysql-connector-java" % "5.1.26"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+         
   )
 
 }
