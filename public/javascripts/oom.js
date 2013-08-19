07a9b48ab42a2c1c8014e6eb3bf530ad1ba67e00
Matthew Frost <mf@matthewfrost.com>
2013-08-18 21:30:46 -0400

    index body content

---
+++ public/javascripts/oom.js
@@ -0,0 +1,8 @@
+function mailto() {
+	ML="f>o@eh=p./rcl nti\"m:<a";
+	MI="DE=5:406ABE@<?2C@>02327?@2>2B4?4:8;2BA1@>02327?@2>2B4?4:8;2BD9E1";
+	OT="";
+	for (j=0;j<MI.length;j++) {
+		OT+=ML.charAt(MI.charCodeAt(j)-48);
+	} document.write(OT);
+}
