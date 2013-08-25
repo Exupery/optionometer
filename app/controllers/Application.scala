21979e2db368f57c8f323220355614198f5669f8
Matthew Frost <mf@matthewfrost.com>
2013-08-25 19:26:16 -0400

    routes for screening with or w/o params

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,13 +9,19 @@ object Application extends Controller {
     Ok(views.html.index())
   }
   
-  def screener = Action {
+  def screenerNoParams = Action {
+    screen("all", "all")
     Ok(views.html.screener())
   }
   
-  def screen(strat: String, und: String, moneyness: Option[String], minDays: Option[Int], maxDays: Option[Int]) = Action {
-    println(strat, und, moneyness, minDays, maxDays)	//DELME
+  def screenerWithParams(strat: String, und: String, moneyness: Option[String], minDays: Option[Int], maxDays: Option[Int]) = Action {
+    screen(strat, und, moneyness, minDays, maxDays)
     Ok(views.html.screener())
+  }
+  
+  def screen(strat: String, und: String, moneyness: Option[String]=None, minDays: Option[Int]=None, maxDays: Option[Int]=None) {
+	//TODO perform screen
+    println(strat, und, moneyness, minDays, maxDays)	//DELME  
   }
   
   def disclaimer = Action {
