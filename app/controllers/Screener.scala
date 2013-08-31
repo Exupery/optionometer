11cb24b96a84e03c34494f38af696bd4c520053c
Matthew Frost <mf@matthewfrost.com>
2013-08-31 14:40:58 -0400

    run SQL with anorm

--- app/controllers/Screener.scala
+++ app/controllers/Screener.scala
@@ -1,6 +1,9 @@
 package controllers
 
+import anorm._ 
 import play.api._
+import play.api.Play.current
+import play.api.db.DB
 import play.api.mvc._
 
 object Screener extends Controller {
@@ -17,8 +20,24 @@ object Screener extends Controller {
   
   def screen(strat: String, und: String, moneyness: Option[String]=None, minDays: Option[Int]=None, maxDays: Option[Int]=None) {
 	//TODO perform screen
+    val u = "MSFT"
+	val qry = "SELECT COUNT(*) FROM options WHERE underlier={underlier}"
+	val sql = SQL(qry).on("underlier"->u)
+	val foo = runQuery(sql)
+//    val foo = DB.withConnection { implicit c =>
+//      sql().map { row =>
+//      	println(row)	//DELME
+//      	row
+//      }.toList  
+//    }
+    println(foo.getClass)	//DELME
+    println(foo)	//DELME
     
     println(strat, und, moneyness, minDays, maxDays)	//DELME  
-  }  
+  }
+  
+  def runQuery(sql: SimpleSql[anorm.Row]): List[Row] = {
+    DB.withConnection(implicit c => sql().toList)
+  }
 
 }
