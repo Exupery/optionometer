4b60e1d50c7b1ba7c763be79cf6a3bf70e357571
Matthew Frost <mf@matthewfrost.com>
2013-09-07 19:15:11 -0400

    refactored strike and money clauses

--- app/controllers/Screener.scala
+++ app/controllers/Screener.scala
@@ -19,24 +19,18 @@ object Screener extends Controller {
   }
   
   def screen(strat: String, und: String, moneyness: Option[String]=None, minDays: Option[Int]=None, maxDays: Option[Int]=None): List[TwoLegTrade] = {
-	println(strat, und, moneyness, minDays, maxDays)	//DELME  
-//    val u = "MSFT"	//TODO get from params
-//    val cp = "C"	//TODO get from params
-//	val qry = """
-//	  	SELECT * FROM twolegs WHERE FROM_UNIXTIME(expires)>NOW() AND
-//		underlier={underlier} AND
-//		callOrPut={callOrPut} AND
-//		longStrike<shortStrike LIMIT 5;
-//	  """	//TODO: revmove limit
-//	val sql = SQL(qry).on("underlier"->u, "callOrPut"->cp)
-//	val base = "SELECT * FROM twolegs WHERE FROM_UNIXTIME(expires)>NOW()"
-	val base = "SELECT * FROM twolegs WHERE FROM_UNIXTIME(expires)>NOW() AND " +
-	  strikeAndType(strat) + " " + money(moneyness.getOrElse("any"))
-	println(base)	//DELME
+	val limit = 25	//TODO: adjust limit
+    println(strat, und, moneyness, minDays, maxDays)	//DELME  
+	val query = {
+	  "SELECT * FROM twolegs WHERE FROM_UNIXTIME(expires)>NOW() AND " +
+	  strikeClause(strat.toLowerCase) + " AND " +
+	  money(strat.toLowerCase, moneyness.getOrElse("any"))
+	}
+	println(query)	//DELME
 	val sql: SimpleSql[Row] = if (und.equalsIgnoreCase("all")) {
-	  SQL(base + " LIMIT 25").asSimple
+	  SQL(query + " LIMIT {limit}").on("limit"->limit)
 	} else {
-	  SQL(base+" AND underlier={underlier} LIMIT 25").on("underlier"->und)
+	  SQL(query+" AND underlier={underlier} LIMIT {limit}").on("underlier"->und, "limit"->limit)
 	}
     val trades: List[TwoLegTrade] = runQuery(sql).map { row =>
       new BullCall(row)	
@@ -44,29 +38,23 @@ object Screener extends Controller {
 	return trades
   }
   
-  def strikeAndType(strat: String): String = {
-    strat.toLowerCase match {
-	  case "bullcalls" => "longStrike < shortStrike AND callOrPut='C'"
-	  case "bullputs" => "longStrike < shortStrike AND callOrPut='P'"
-      case "bullish" => "longStrike < shortStrike"
-	  case "bearcalls" => "longStrike > shortStrike AND callOrPut='C'"
-	  case "bearputs" => "longStrike > shortStrike AND callOrPut='P'"
-      case "bearish" => "longStrike > shortStrike"
-	  case _ => "longStrike != shortStrike"
-	}
+  def strikeClause(strat: String): String = {
+    val opr = strat.toLowerCase match {
+      case s if "bull.*".r.findFirstIn(s).isDefined => "<"
+      case s if "bear.*".r.findFirstIn(s).isDefined => ">"
+      case _ => "!="  
+    }
+    return "longStrike " + opr + " shortStrike"
   }
   
-  def money(moneyness: String): String = {
-    //TODO: handle itm/otm
-    val between = moneyness.toLowerCase match {
-      case "itm" => (0.75, 1.25)
-      case "atm" => (0.975, 1.025)
-      case "otm" => (0.75, 1.25)
-      case _ => (0.75, 1.25)
+  def money(strat: String, moneyness: String): String = {
+    moneyness.toLowerCase match {
+      case "itm" => if (strat.startsWith("bull")) "undLast > shortStrike" else "undLast < shortStrike"
+      case "otm" => if (strat.startsWith("bull")) "undLast < shortStrike" else "undLast > shortStrike"
+      case "ntm" => "shortStrike BETWEEN (undLast*0.975) AND (undLast*1.025)"
+      case _ => "shortStrike BETWEEN (undLast*0.75) AND (undLast*1.25)"
       
     }
-    return "AND longStrike BETWEEN (undLast*"+between._1+") AND (undLast*"+between._2+") "+
-			"AND shortStrike BETWEEN (undLast*"+between._1+") AND (undLast*"+between._2+")"
   }
   
   def runQuery(sql: SimpleSql[Row]): List[Row] = {
