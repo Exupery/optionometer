942e35e60b95c5e591c811a46ad3c0883c1530cd
Matthew Frost <mf@matthewfrost.com>
2013-09-08 18:00:14 -0400

    moved tests to own package

---
+++ test/test/BullCallSpec.scala
@@ -0,0 +1,18 @@
+package test
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class BullCallSpec extends Specification {
+  
+  "a BullCall" should {
+    
+    "have a long strike lower than the short strike" in {
+      "" must equalTo("")
+    }
+    
+  }
+  
+}
