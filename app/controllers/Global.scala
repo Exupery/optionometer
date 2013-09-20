ce9876d391ab2639b1dd4e113ef17b0604700926
Matthew Frost <mf@matthewfrost.com>
2013-09-19 21:04:11 -0400

    added 404 page

---
+++ app/controllers/Global.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+
+object Global extends GlobalSettings {
+  
+  override def onHandlerNotFound(request: RequestHeader): Result = {
+    NotFound(views.html.notfound(request.path))
+  }
+
+}
