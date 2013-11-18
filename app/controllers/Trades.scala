fbcff4ffa4678abc70b53cc7636a1951ecbd0862
Matthew Frost <mf@matthewfrost.com>
2013-11-17 19:07:56 -0500

    parse trade parameters

--- app/controllers/Trades.scala
+++ app/controllers/Trades.scala
@@ -17,11 +17,48 @@ object Trades extends Controller {
       und: String,
       year: String,
       month: String,
-      leg1: String,
-      leg2: String) = Action {
-    //MSFT/2014/01/L3400P/S3500P
-    println(und, year, month, leg1, leg2)	//DELME
+      legs: String) = Action {
+    //MSFT/2013/12/L34.00P-S35.00P
+    println(und, year, month, legs)	//DELME
+    val params = new TradeParams(und, year, month, legs)
+    println(params.underlier, params.expiryYear, params.expiryMonth, params.legs)	//DELME
     Ok
   }
+  
+  class TradeParams(
+      und: String,
+      year: String,
+      month: String,
+      legStr: String) {
+    val underlier = und
+    val expiryYear: Int = safeInt(year)
+    val expiryMonth: Int = safeInt(month)
+    val legs: List[Leg] = parseLegs(legStr)
+    
+    private def parseLegs(str: String): List[Leg] = {
+      return str.toUpperCase.split("-").filter(_.length>2).map { leg =>
+        val strike: BigDecimal = safeBigDecimal(leg.substring(1, leg.length-1))
+        Leg(leg.startsWith("L"), strike, leg.endsWith("C"))
+      }.toList
+    }
+    
+    private def safeInt(str: String): Int = {
+      try {
+        str.toInt
+      } catch {
+        case e: NumberFormatException => 0
+      }
+    }
+    
+    private def safeBigDecimal(str: String): BigDecimal = {
+      try {
+        BigDecimal(str)
+      } catch {
+        case e: NumberFormatException => BigDecimal("0")
+      }
+    }
+  }
+  
+  case class Leg(isLong: Boolean, strike: BigDecimal, callOrPut: Boolean)
 
 }
